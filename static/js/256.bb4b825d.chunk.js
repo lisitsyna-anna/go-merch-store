566f06d6734a4cab0239b27f45d0115567d19741
lisitsyna-anna <104447922+lisitsyna-anna@users.noreply.github.com>
2022-12-22 11:16:40 +0000

    Deploying to gh-pages from @ lisitsyna-anna/app-with-routing@685c8e852c657b63876ef0eb28a3f5900551dcaa 🚀

---
+++ static/js/256.bb4b825d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[256],{256:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=256.bb4b825d.chunk.js.map
---
+++ static/js/544.6ee4d8fe.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,t,i){i.r(t);var a=i(184);t.default=function(){return(0,a.jsxs)("main",{children:[(0,a.jsx)("h1",{children:"Welcome"}),(0,a.jsx)("img",{src:"https://via.placeholder.com/960x240",alt:""}),(0,a.jsx)("p",{children:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, laboriosam placeat incidunt rem illum animi nemo quibusdam quia voluptatum voluptate."})]})}}}]);
+//# sourceMappingURL=544.6ee4d8fe.chunk.js.map
